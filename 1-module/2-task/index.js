d27a0cda80c1d2283a98217504b50a654dde5bca
natty-dv <natalushka94@mail.ru>
2024-05-26 17:28:22 +0300

    Решение задач из Занятия 1

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name !== null) {
+    return (name.length >= 4) && (name.indexOf(' ') == -1);
+  } else {
+    return false;
+  }
 }
 
 function sayHello() {
--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,9 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str === "") {
+    return "";
+  }
+
+  let name = str[0].toUpperCase() + str.slice(1);
+  return name;
 }
+
