dac84e5add2c3b25637e4864b781f6f496a51189
natty-dv <natalushka94@mail.ru>
2024-05-30 02:25:39 +0300

    Решение задач из занятия 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,20 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
+
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if ((typeof salaries[key] == 'number') && (isFinite(salaries[key])) && (!isNaN(salaries[key]))) {
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
+
+console.log(sumSalary(salaries));
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,15 @@
 let calculator = {
-  // ваш код
-};
+  read (one, two) {
+    this.a = +one;
+    this.b = +two;
+  },
+  sum () {
+    return this.a + this.b;
+  },
+  mul () {
+    return this.a * this.b;
+  }
+}
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
