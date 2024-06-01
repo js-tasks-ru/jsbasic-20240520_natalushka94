78143346d4803cd16aa40a2d8cbb509bbf2dc0e5
natty-dv <natalushka94@mail.ru>
2024-06-01 16:22:17 +0300

    Занятие 2. Корректировка

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,20 +1,9 @@
-let salaries = {
-  John: 1000,
-  Ann: 1600,
-  Pete: 1300,
-  month: 'December',
-  currency: 'USD',
-  isPayed: false
-};
-
 function sumSalary(salaries) {
   let sum = 0;
   for (let key in salaries) {
-    if ((typeof salaries[key] == 'number') && (isFinite(salaries[key])) && (!isNaN(salaries[key]))) {
+    if ((typeof salaries[key] == 'number') && (isFinite(salaries[key]))) {
       sum += salaries[key];
     }
   }
   return sum;
 }
-
-console.log(sumSalary(salaries));
