87162c02195e870bff26048958c21f91e90475d3
natty-dv <natalushka94@mail.ru>
2024-06-04 12:21:14 +0300

    Решение задач из занятия 3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  let newstr;
+  newstr = str.split(' ');
+  for (let i = 0; i < newstr.length; i++) {
+    newstr[i] = Number(newstr[i]);
+  }
+  let filtered = newstr.filter(Boolean);
+  let result = {
+    min: Math.min(...filtered),
+    max: Math.max(...filtered)
+  };
+  return result;
 }
